d36dd1da7a55d40d77b70f046bcd9cd4503830b1
Rubyroy12 <ibrakipz7@gmail.com>
2021-06-16 00:31:07 +0300

    change smtp port to 2525

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY')   
      #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 25
+    MAIL_PORT = 2525
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") 
