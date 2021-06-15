cc78080544fd70024fd2e7d6fe982de1e4a7da9a
Rubyroy12 <ibrakipz7@gmail.com>
2021-06-15 19:47:06 +0300

    add production

--- config.py
+++ config.py
@@ -11,8 +11,10 @@ class Config():
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") 
 
 
-class prodConfig(Config):
-     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
 
 
 class TestConfig(Config):
