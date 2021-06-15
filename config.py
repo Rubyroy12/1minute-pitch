1c16643811d75e4f02d7975c9a4eb6afe57b6f22
Rubyroy12 <ibrakipz7@gmail.com>
2021-06-16 00:40:34 +0300

    remove mail port white space

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config():
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:Royal12@localhost/pitch'
     SECRET_KEY = os.environ.get('SECRET_KEY')   
      #  email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 2525
+    MAIL_SERVER='smtp.googlemail.com'
+    MAIL_PORT=587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") 
